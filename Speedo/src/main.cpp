07c46aa4b4b2cb6e35aee6f98100666c8e69f221
Brett Adams <brett.whynot@gmail.com>
2023-11-10 17:25:23 +1000

    updates

--- Speedo/src/main.cpp
+++ Speedo/src/main.cpp
@@ -3,65 +3,73 @@
 #include <M5Unified.h>
 #include <WiFi.h>
 #include <esp_now.h>
+#include <Preferences.h>
 
-// ESP CAN is xD4:D4:DA:9D:FD:E4
-uint8_t senderAddress[] = {0xD4, 0xD4, 0xDA, 0x9D, 0xFD, 0xE4};
+Preferences preferences;
+
+u8_t senderAddress[] = {0xD4, 0xD4, 0xDA, 0x9D, 0xFD, 0xE4};
 esp_now_peer_info_t peerInfo;
 
 bool render = true;
-long oldPosition = 0;
-int value = 0;
-int oldvalue = 0;
-long lasttime = 0;
+u32_t oldPosition = 0;
+s16_t value = 0;
+s16_t old_value = 0;
+u32_t next_time = 0;
 u8_t page = 0;
 
-#define PAGES 7
+#define PAGES 8
 
 // Receive data
-void OnDataRecv(const uint8_t *mac, const uint8_t *data, int len)
+void OnDataRecv(const u8_t *mac, const u8_t *data, int len)
 {
-    if (millis() < lasttime)
-        return;
-    lasttime = millis() + 99;
-    oldvalue = value;
-    M5Dial.Display.setTextSize(1);
-    M5Dial.Display.drawString("-----" + String(data[0], HEX) + ":" + String(data[1], HEX) + ":" + String(data[2], HEX) + ":" + String(data[3], HEX) + "-----", 120, 180);
+    if (millis() < next_time)
+        return; // Rate Limit
+    next_time = millis() + 99;
+    old_value = value;
+
     switch (page)
     {
-    case (0): // Speed 12|12
+    case 0: // Speed 12|12
         value = ((data[1] >> 4) + (data[2] << 4)) * 0.8 - 400;
         break;
 
-    case (1): // AC Left 0|5
+    case 1: // AC Left 0|5
         value = (data[0] & B00011111) * 5 + 150;
         break;
 
-    case (2): // AC Right 8|5
+    case 2: // AC Right 8|5
         value = (data[1] & B00011111) * 5 + 150;
         break;
 
-    case (3): // Front Torque 21|13
-        value = ((data[2] & B11111000 >> 3) + (data[3] << 8)) * 2.22;
+    case 3:                                                    // Front Torque 21|13
+        value = (data[1] + (data[2] & B00001111 << 8)) * 2.22; //+ (data[3] & B00000000 << 8)
         break;
 
-    case (4): // Rear Torque 21|13
-        value = ((data[2] & B11111000 >> 3) + (data[3] << 8)) * 2.22;
+    case 4:                                                    // Rear Torque 21|13
+        value = (data[1] + (data[2] & B00001111 << 8)) * 2.22; // + (data[3] & B10000000 << 8)
         break;
 
-    case (5): // HV Battery Voltage 0|16
-        value = ((data[0]) + (data[1] << 8)) * 0.1;
+    case 5: // HV Battery Voltage 0|16
+        value = (data[0] | data[1] << 8) * 0.1;
         break;
 
-    case (6): // HV Battery Current 16|16
-        value = ((data[2]) + (data[3] << 8)) * -1;
+    case 6: // HV Battery Current 16|16
+        value = (data[2] | data[3] << 8);
+        break;
+    case 7: // Indicators
+        value = data[0] >> 4;
         break;
     }
-    render = value != oldvalue;
+    render = value != old_value;
+
+    // This is for debugging only
+    M5Dial.Display.setTextSize(1);
+    M5Dial.Display.drawString("     " + String(data[0], HEX) + ":" + String(data[1], HEX) + ":" + String(data[2], HEX) + ":" + String(data[3], HEX) + "     ", 120, 180);
 }
 
 // Craft filters based on CAN Bus ID
 
-const u16_t id_speed = 599, id_hvac = 755, id_fronttorque = 469, id_reartorque = 472, id_hvbattery = 306;
+const u16_t id_speed = 599, id_hvac = 755, id_fronttorque = 469, id_reartorque = 472, id_hvbattery = 306, id_lights = 1013;
 
 void changePage()
 {
@@ -71,65 +79,79 @@ void changePage()
     switch (page)
     {
     case 0:
-        esp_now_send(senderAddress, (uint8_t *)&id_speed, 2);
+        esp_now_send(senderAddress, (u8_t *)&id_speed, 2);
         M5Dial.Display.drawString("     Speed     ", 120, 40);
         // M5Dial.Display.drawString("   KM/H   ", 120, 200);
         break;
     case 1:
-        esp_now_send(senderAddress, (uint8_t *)&id_hvac, 2);
+        esp_now_send(senderAddress, (u8_t *)&id_hvac, 2);
         M5Dial.Display.drawString("   AC Left   ", 120, 40);
         // M5Dial.Display.drawString("  Celsius  ", 120, 200);
         break;
     case 2:
-        esp_now_send(senderAddress, (uint8_t *)&id_hvac, 2);
+        esp_now_send(senderAddress, (u8_t *)&id_hvac, 2);
         M5Dial.Display.drawString("   AC Right   ", 120, 40);
         // M5Dial.Display.drawString("  Celsius  ", 120, 200);
         break;
     case 3:
-        esp_now_send(senderAddress, (uint8_t *)&id_fronttorque, 2);
+        esp_now_send(senderAddress, (u8_t *)&id_fronttorque, 2);
         M5Dial.Display.drawString("   Front Torque   ", 120, 40);
         // M5Dial.Display.drawString("  Nm  ", 120, 200);
         break;
     case 4:
-        esp_now_send(senderAddress, (uint8_t *)&id_reartorque, 2);
+        esp_now_send(senderAddress, (u8_t *)&id_reartorque, 2);
         M5Dial.Display.drawString("   Rear Torque   ", 120, 40);
         // M5Dial.Display.drawString("  Nm  ", 120, 200);
         break;
     case 5:
-        esp_now_send(senderAddress, (uint8_t *)&id_hvbattery, 2);
+        esp_now_send(senderAddress, (u8_t *)&id_hvbattery, 2);
         M5Dial.Display.drawString("   HV Voltage   ", 120, 40);
         // M5Dial.Display.drawString("  Nm  ", 120, 200);
         break;
     case 6:
-        esp_now_send(senderAddress, (uint8_t *)&id_hvbattery, 2);
+        esp_now_send(senderAddress, (u8_t *)&id_hvbattery, 2);
         M5Dial.Display.drawString("   HV Current   ", 120, 40);
+        // M5Dial.Display.drawString("  Nm  ", 120, 200);
+        break;
+    case 7:
+        esp_now_send(senderAddress, (u8_t *)&id_lights, 2);
+        M5Dial.Display.drawString("   Indicators   ", 120, 40);
         // M5Dial.Display.drawString("  Nm  ", 120, 200);
         break;
     }
     value = INT_MIN;
-    lasttime = 0;
+    next_time = 0;
     render = false;
 }
 
 void setup()
 {
-    // put your setup code here, to run once:
+    // Setup WiFi for ESP Now
+    WiFi.mode(WIFI_STA);
+
+    // Setup persistant storage
+    preferences.begin("dial", false);
+    page = preferences.getUChar("page", 0);
+
+    // Setup M5Dial
     auto cfg = M5.config();
     M5Dial.begin(cfg, true, false);
+
     M5Dial.Display.setRotation(3);
     M5Dial.Display.setTextDatum(middle_center);
     M5Dial.Display.setFont(&FreeSansBold12pt7b);
 
-    WiFi.mode(WIFI_STA);
+    // Start ESP Now
     if (esp_now_init() != ESP_OK)
     {
         M5Dial.Display.setColor(RED);
-        M5Dial.Display.drawString("WiFi Failed", 120, 120);
+        M5Dial.Display.drawString("ESPNOW Failed", 120, 120);
         delay(1000);
+        ESP.restart();
     }
-
     esp_now_register_recv_cb(OnDataRecv);
 
+    // Setup ESP Now peer
     memcpy(peerInfo.peer_addr, senderAddress, 6);
     peerInfo.channel = 0;
     peerInfo.encrypt = false;
@@ -140,23 +162,48 @@ void setup()
         delay(1000);
     }
 
+    // Draw the page
     changePage();
 }
 
 void loop()
 {
-    long newPosition = M5Dial.Encoder.read() / 4;
+    u16_t newPosition = M5Dial.Encoder.read() / 4;
     s8_t change = newPosition - oldPosition;
     if (change != 0)
     {
         page = (PAGES + page + change) % PAGES;
+        preferences.putUChar("page", page);
         oldPosition = newPosition;
         changePage();
     }
     if (render)
     {
-        M5Dial.Display.setTextSize(4);
-        M5Dial.Display.drawString("  " + String(value / 10) + "." + String(value % 10) + "  ", 120, 120);
+        switch (page)
+        {
+        case (7):
+            M5Dial.Display.setTextSize(6);
+            if (value & B1)
+                M5Dial.Display.drawString("<", 120, 60);
+            else if (value & B10)
+                M5Dial.Display.drawString("<<", 120, 60);
+            if (value & B100)
+                M5Dial.Display.drawString(">", 120, 60);
+            else if (value & B1000)
+                M5Dial.Display.drawString(">>", 120, 60);
+            break;
+        default:
+            M5Dial.Display.setTextSize(4);
+            if (value < 2000) // Value less than 200.0
+            {
+                M5Dial.Display.drawString("  " + String(value / 10) + "." + String(abs(value) % 10) + "  ", 120, 120);
+            }
+            else // Value 200.0 or higher
+            {
+                M5Dial.Display.drawString("    " + String(value / 10) + "    ", 120, 120);
+            }
+            break;
+        }
         render = false;
     }
 }
