72a761edf7084f25046449a2ba03d39f432eada6
Brett Adams <brett.whynot@gmail.com>
2023-11-16 08:08:01 +1000

    So much works

--- Speedo/src/main.cpp
+++ Speedo/src/main.cpp
@@ -12,8 +12,8 @@ esp_now_peer_info_t peerInfo;
 
 bool render = false;
 int32_t oldPosition = 0;
-int16_t value = INT16_MAX;
-int16_t old_value = INT16_MAX;
+int32_t value = INT32_MAX;
+int32_t old_value = INT32_MAX;
 unsigned long next_time = 0;
 uint8_t page = 0;
 bool dark = true;
@@ -29,24 +29,25 @@ void ExtractValue(u8_t start, u8_t length, u8_t *data)
     }
 }
 
-#define PAGES 11
+#define PAGES 13
 
 #define PAGE_SPEED 0
-#define PAGE_FRONT_POWER 1
-#define PAGE_FRONT_AMPS 2
-#define PAGE_REAR_POWER 3
-#define PAGE_REAR_AMPS 4
-#define PAGE_HV_BATTERY_VOLTAGE 5
-#define PAGE_HV_BATTERY_CURRENT 6
-#define PAGE_INDICATORS 7
-#define PAGE_CABIN_TEMP 8
-#define PAGE_ACTUAL_TEMP 9
-#define PAGE_OUTSIDE_TEMP 10
+#define PAGE_HV_BATTERY_POWER 1
+#define PAGE_HV_BATTERY_VOLTAGE 2
+#define PAGE_HV_BATTERY_CURRENT 3
+#define PAGE_REAR_TORQUE 4
+#define PAGE_REAR_POWER 5
+#define PAGE_REAR_AMPS 6
+#define PAGE_FRONT_TORQUE 7
+#define PAGE_FRONT_POWER 8
+#define PAGE_FRONT_AMPS 9
+#define PAGE_CABIN_TEMP 10
+#define PAGE_ACTUAL_TEMP 11
+#define PAGE_OUTSIDE_TEMP 12
 
+#define PAGE_INDICATORS 13
 #define PAGE_AC_LEFT 101
 #define PAGE_AC_RIGHT 102
-#define PAGE_FRONT_TORQUE 103
-#define PAGE_REAR_TORQUE 105
 
 // Receive data
 void OnDataRecv(const u8_t *mac, const u8_t *data, int len)
@@ -80,21 +81,23 @@ void OnDataRecv(const u8_t *mac, const u8_t *data, int len)
         decimal = true;
         break;
     case PAGE_FRONT_POWER: // 0|11@1- (0.5,0) [-512|511.5]
-        value = (data[0] | data[1] & B11 << 8) * (data[1] & 4 ? -5 : 5);
+        value = (s16_t)(data[0] << 5 | data[1] << 13) / 32 * 5;
         decimal = true;
         break;
     case PAGE_FRONT_TORQUE: // 21|13@1- (0.222,0) [-909.312|909.09] "NM"  Receiver
         // value = (data[1] + (data[2] & B00001111 << 8)) * 2.22; //+ (data[3] & B00000000 << 8)
-        value = (data[2] >> 5 | data[3] << 3 | data[4] & 1 << 11) * (data[4] & 2) ? 2.22 : -2.22;
+        // value = (data[2] >> 5 | data[3] << 3 | data[4] & 1 << 11) * (data[4] & 2) ? 2.22 : -2.22;
+        value = (s16_t)(data[2] >> 2 | data[3] << 6 | data[4] << 14) * 0.2775;
         decimal = true;
         break;
     case PAGE_REAR_POWER: // 0|11@1- (0.5,0) [-512|511.5]
-        value = (data[0] | data[1] & B11 << 8) * (data[1] & 4 ? -5 : 5);
+        value = (s16_t)(data[0] << 5 | data[1] << 13) / 32 * 5;
         decimal = true;
         break;
     case PAGE_REAR_TORQUE: // 21|13@1- (0.222,0) [-909.312|909.09] "NM"  Receiver
         // value = (data[1] + (data[2] & B00001111 << 8)) * 2.22; // + (data[3] & B10000000 << 8)
-        value = (data[2] >> 5 | data[3] << 3 | data[4] & 1 << 11) * (data[4] & 2) ? 2.22 : -2.22;
+        // value = (data[2] >> 5 | data[3] << 3 | data[4] & 1 << 11) * (data[4] & 2) ? 2.22 : -2.22;
+        value = (s16_t)(data[2] >> 2 | data[3] << 6 | data[4] << 14) * 0.2775;
         decimal = true;
         break;
     case PAGE_FRONT_AMPS: // 11|11@1+ (1,0) [0|2047]
@@ -105,13 +108,17 @@ void OnDataRecv(const u8_t *mac, const u8_t *data, int len)
         value = (data[1] >> 5 | data[2] << 3) * 10;
         decimal = false;
         break;
+    case PAGE_HV_BATTERY_POWER: // Voltage * Current / 1000
+        value = -round((data[0] | data[1] << 8) * (int16_t)(data[2] | data[3] << 8) / 10000) / 10;
+        decimal = true;
+        break;
     case PAGE_HV_BATTERY_VOLTAGE: // 0|16@1+ (0.01,0) [0|655.35]
         value = (data[0] | data[1] << 8) * 0.1;
         decimal = true;
         break;
     case PAGE_HV_BATTERY_CURRENT: // 16|16@1- (-0.1,0) [-3276.7|3276.7]
-        value = (data[2] | (data[3] & 127) << 8) * (data[3] & 128 ? -10 : 10);
-        decimal = false;
+        value = -(int16_t)(data[2] | data[3] << 8);
+        decimal = true;
         break;
     case PAGE_INDICATORS: // 4|4?
         value = data[0] & 15;
@@ -121,7 +128,7 @@ void OnDataRecv(const u8_t *mac, const u8_t *data, int len)
         decimal = true;
         break;
     case PAGE_ACTUAL_TEMP: // 1|8@1- (1,-40)
-        value = (data[0] >> 1) * 10 - 400;
+        value = (s8_t)(data[0] >> 1 | data[1] << 7) * 10 - 400;
         decimal = false;
         break;
     case PAGE_OUTSIDE_TEMP: // 24|8@1+ (0.5,-40)
@@ -162,78 +169,95 @@ void drawPage()
     {
     case PAGE_SPEED:
         esp_now_send(senderAddress, (u8_t *)&id_speed, 2);
-        M5Dial.Display.drawString("       Speed       ", 120, 50);
-        M5Dial.Display.drawString("   KM/H   ", 120, 180);
+        M5Dial.Display.drawString("Speed", 120, 50);
+        M5Dial.Display.drawString("KM/H", 120, 180);
         break;
     case PAGE_AC_LEFT:
         esp_now_send(senderAddress, (u8_t *)&id_hvac_request, 2);
-        M5Dial.Display.drawString("   AC Left   ", 120, 50);
-        M5Dial.Display.drawString("  Celsius  ", 120, 180);
+        M5Dial.Display.drawString("AC Left", 120, 50);
+        M5Dial.Display.drawString("Celsius", 120, 180);
         break;
     case PAGE_AC_RIGHT:
         esp_now_send(senderAddress, (u8_t *)&id_hvac_request, 2);
-        M5Dial.Display.drawString("   AC Right   ", 120, 50);
-        M5Dial.Display.drawString("  Celsius  ", 120, 180);
+        M5Dial.Display.drawString("AC Right", 120, 50);
+        M5Dial.Display.drawString("Celsius", 120, 180);
         break;
     case PAGE_FRONT_POWER:
         esp_now_send(senderAddress, (u8_t *)&id_front_power, 2);
-        M5Dial.Display.drawString("   Front Power   ", 120, 50);
-        M5Dial.Display.drawString("   kW   ", 120, 180);
+        M5Dial.Display.drawString("Front", 120, 20);
+        M5Dial.Display.drawString("Power", 120, 50);
+        M5Dial.Display.drawString("kW", 120, 180);
         break;
     case PAGE_REAR_POWER:
         esp_now_send(senderAddress, (u8_t *)&id_rear_power, 2);
-        M5Dial.Display.drawString("   Rear Power   ", 120, 50);
-        M5Dial.Display.drawString("   kW   ", 120, 180);
+        M5Dial.Display.drawString("Rear", 120, 20);
+        M5Dial.Display.drawString("Power", 120, 50);
+        M5Dial.Display.drawString("kW", 120, 180);
         break;
     case PAGE_FRONT_TORQUE:
         esp_now_send(senderAddress, (u8_t *)&id_front_torque, 2);
-        M5Dial.Display.drawString("   Front Torque   ", 120, 50);
-        M5Dial.Display.drawString("  Nm  ", 120, 180);
+        M5Dial.Display.drawString("Front", 120, 20);
+        M5Dial.Display.drawString("Torque", 120, 50);
+        M5Dial.Display.drawString("Nm", 120, 180);
         break;
     case PAGE_REAR_TORQUE:
         esp_now_send(senderAddress, (u8_t *)&id_rear_torque, 2);
-        M5Dial.Display.drawString("   Rear Torque   ", 120, 50);
-        M5Dial.Display.drawString("    Nm    ", 120, 180);
+        M5Dial.Display.drawString("Rear", 120, 20);
+        M5Dial.Display.drawString("Torque", 120, 50);
+        M5Dial.Display.drawString("Nm", 120, 180);
         break;
     case PAGE_FRONT_AMPS:
         esp_now_send(senderAddress, (u8_t *)&id_front_amps, 2);
-        M5Dial.Display.drawString("   Front Motor   ", 120, 50);
-        M5Dial.Display.drawString("   Amps   ", 120, 180);
+        M5Dial.Display.drawString("Front", 120, 20);
+        M5Dial.Display.drawString("Motor", 120, 50);
+        M5Dial.Display.drawString("Amps", 120, 180);
         break;
     case PAGE_REAR_AMPS:
         esp_now_send(senderAddress, (u8_t *)&id_rear_amps, 2);
-        M5Dial.Display.drawString("   Rear Motor   ", 120, 50);
-        M5Dial.Display.drawString("   Amps   ", 120, 180);
+        M5Dial.Display.drawString("Rear", 120, 20);
+        M5Dial.Display.drawString("Motor", 120, 50);
+        M5Dial.Display.drawString("Amps", 120, 180);
+        break;
+    case PAGE_HV_BATTERY_POWER:
+        esp_now_send(senderAddress, (u8_t *)&id_hv_battery, 2);
+        M5Dial.Display.drawString("HV", 120, 20);
+        M5Dial.Display.drawString("Power", 120, 50);
+        M5Dial.Display.drawString("kW", 120, 180);
         break;
     case PAGE_HV_BATTERY_VOLTAGE:
         esp_now_send(senderAddress, (u8_t *)&id_hv_battery, 2);
-        M5Dial.Display.drawString("   HV Voltage   ", 120, 50);
-        M5Dial.Display.drawString("   Volts   ", 120, 180);
+        M5Dial.Display.drawString("HV", 120, 20);
+        M5Dial.Display.drawString("Voltage", 120, 50);
+        M5Dial.Display.drawString("Volts", 120, 180);
         break;
     case PAGE_HV_BATTERY_CURRENT:
         esp_now_send(senderAddress, (u8_t *)&id_hv_battery, 2);
-        M5Dial.Display.drawString("   HV Current   ", 120, 50);
-        M5Dial.Display.drawString("   Amps   ", 120, 180);
+        M5Dial.Display.drawString("HV", 120, 20);
+        M5Dial.Display.drawString("Current", 120, 50);
+        M5Dial.Display.drawString("Amps", 120, 180);
         break;
     case PAGE_INDICATORS:
         esp_now_send(senderAddress, (u8_t *)&id_lights, 2);
-        M5Dial.Display.drawString("   Indicators   ", 120, 50);
-        M5Dial.Display.drawString("            ", 120, 180);
+        M5Dial.Display.drawString("Indicators", 120, 50);
         break;
     case PAGE_CABIN_TEMP:
         esp_now_send(senderAddress, (u8_t *)&id_hvac_status, 4);
-        M5Dial.Display.drawString("   Cabin Temp   ", 120, 50);
-        M5Dial.Display.drawString("  Celsius  ", 120, 180);
+        M5Dial.Display.drawString("Cabin", 120, 50);
+        M5Dial.Display.drawString("Temp", 120, 20);
+        M5Dial.Display.drawString("Celsius", 120, 180);
         break;
     case PAGE_ACTUAL_TEMP:
         esp_now_send(senderAddress, (u8_t *)&id_ths, 2);
-        M5Dial.Display.drawString("   Inside Temp   ", 120, 50);
-        M5Dial.Display.drawString("  Celsius  ", 120, 180);
+        M5Dial.Display.drawString("Inside", 120, 50);
+        M5Dial.Display.drawString("Temp", 120, 20);
+        M5Dial.Display.drawString("Celsius", 120, 180);
         break;
     case PAGE_OUTSIDE_TEMP:
         esp_now_send(senderAddress, (u8_t *)&id_front_sensor, 2);
-        M5Dial.Display.drawString("   Outside Temp   ", 120, 50);
-        M5Dial.Display.drawString("  Celsius  ", 120, 180);
+
+        M5Dial.Display.drawString("Outside", 120, 50);
+        M5Dial.Display.drawString("Temp", 120, 20);
+        M5Dial.Display.drawString("Celsius", 120, 180);
         break;
     }
     value = INT16_MAX;
@@ -367,13 +391,13 @@ void loop()
             break;
         default: // Standard Metrics
             M5Dial.Display.setTextSize(2);
-            if (value >= 2000 | !decimal)
+            if (decimal && (value < 2000) && (value > -1000))
             {
-                M5Dial.Display.drawString("    " + String(value / 10) + "    ", 120, 125, &FreeSansBold24pt7b);
+                M5Dial.Display.drawString("  " + String(value / 10) + "." + String(abs(value) % 10) + "  ", 120, 125, &FreeSansBold24pt7b);
             }
             else
             {
-                M5Dial.Display.drawString("  " + String(value / 10) + "." + String(abs(value) % 10) + "  ", 120, 125, &FreeSansBold24pt7b);
+                M5Dial.Display.drawString("    " + String(value / 10) + "    ", 120, 125, &FreeSansBold24pt7b);
             }
             break;
         }
